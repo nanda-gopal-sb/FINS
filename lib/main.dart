a8166a55745d01c1e141d553e8ddb0a8e211e742
Nandagopal <nandagopal.nsb@gmail.com>
2024-02-16 10:14:21 +0530

    Added Login Page and Some other error fixes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,106 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_login/flutter_login.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
-
+//import 'package:flutter_login/theme.dart';
 void main() {
-  runApp(const FINS());
+  //MaterialApp(home: MyHome()),
+  runApp(const MaterialApp(home: Login()),);
+}
+const users =  {
+  'dribbble@gmail.com': '12345',
+  'hunter@gmail.com': 'hunter',
+};
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginState createState() => _LoginState();
 }
 
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    const Directionality(
+          textDirection: TextDirection.ltr,
+          child: Text('Hello'));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Login Page"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: SizedBox(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('asset/images/flutter-logo.png')),
+              ),
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                // FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => const FINS()));
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            const Text('New User? Create Account')
+          ],
+        ),
+      ),
+    );
+  }
+}
 class FINS extends StatelessWidget {
   const FINS({super.key});
 
@@ -24,20 +120,20 @@ class FINS extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const App(title: 'FINS Home'),
+      home: const HomePage(title: 'FINS Home'),
     );
   }
 }
 
-class App extends StatefulWidget {
-  const App({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
   final String title;
 
   @override
-  State<App> createState() => _AppState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _AppState extends State<App> {
+class _HomePageState extends State<HomePage> {
   int _selectedPageIndex = 1;
 
   void _onItemTapped(int index) {
@@ -89,9 +185,10 @@ class SensorWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double temperature = 39;
+    double temperature = 39; //to be gotten from Sensors
     double moisture = 25;
     double humidity = 85;
+    double soilMosture = 55; 
     return Expanded(
       child: SingleChildScrollView(
         padding: const EdgeInsets.all(30),
@@ -101,6 +198,7 @@ class SensorWidget extends StatelessWidget {
             SensorCard(heading: "Temperature", unit: " °C", value: temperature),
             SensorCard(heading: "Humidity", unit: " %", value: humidity),
             SensorCard(heading: "Moisture", unit: " %", value: moisture),
+            SensorCard(heading: "SoilMosture", unit: " %", value: soilMosture),
           ],
         ),
       ),
@@ -130,7 +228,7 @@ class SensorCard extends StatelessWidget {
       decoration: BoxDecoration(
         boxShadow: [
           BoxShadow(
-            // TODO: Find a better name for shadow color
+            // Find a better name for shadow color
             color: Theme.of(context).colorScheme.tertiary.withAlpha(70),
             blurRadius: 10.0,
             spreadRadius: 0.0,
