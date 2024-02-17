4ea8d976c9da7c5b6c9a7879fc7ff338a14d9b2a
Roopesh <62459820+Roopesh2@users.noreply.github.com>
2024-02-17 07:33:26 +0530

    Fixed ParentDataWidget exception

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:fins/home_page.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_login/flutter_login.dart';
-//import 'package:flutter_login/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 const users = {
   'dribbble@gmail.com': '12345',
@@ -12,11 +11,19 @@ class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  Future<void> asyncSetLoginStateTrue(
+    BuildContext context,
+    VoidCallback callback,
+  ) async {
+    // SharedPreferences pref = await SharedPreferences.getInstance();
+    // pref.setBool("isLoggedIn", true);
+    callback.call();
+  }
+
   @override
   Widget build(BuildContext context) {
     const Directionality(
@@ -24,81 +31,80 @@ class _LoginPageState extends State<LoginPage> {
       child: Text('Hello'),
     );
     return Scaffold(
-      body: Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Center(
-              child: SizedBox(
-                height: 200,
-                child: Image(image: AssetImage('assets/images/icon.png')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Center(
+            child: SizedBox(
+              height: 200,
+              child: Image(image: AssetImage('assets/images/icon.png')),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                hintText: 'Enter valid email id as abc@gmail.com',
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  hintText: 'Enter valid email id as abc@gmail.com',
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              left: 15.0,
+              right: 15.0,
+              top: 15,
+              bottom: 15,
+            ),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'Enter secure password',
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              // FORGOT PASSWORD SCREEN GOES HERE
+            },
+            style: Theme.of(context).textButtonTheme.style!.copyWith(
+                  backgroundColor:
+                      colorToMColor(Theme.of(context).colorScheme.background),
                 ),
-              ),
+            child: const Text(
+              'Forgot Password',
+              style: TextStyle(color: Colors.blue, fontSize: 15),
             ),
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 15.0,
-                right: 15.0,
-                top: 15,
-                bottom: 15,
-              ),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter secure password',
-                ),
-              ),
+          ),
+          Container(
+            height: 50,
+            width: 250,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
             ),
-            TextButton(
-              onPressed: () {
-                // FORGOT PASSWORD SCREEN GOES HERE
-              },
-              style: Theme.of(context).textButtonTheme.style!.copyWith(
-                    backgroundColor:
-                        colorToMColor(Theme.of(context).colorScheme.background),
-                  ),
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextButton(
-                onPressed: () {
+            child: TextButton(
+              onPressed: () async {
+                _LoginPageState().asyncSetLoginStateTrue(context, () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (_) => const HomePage()),
                   );
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
+                });
+              },
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 25),
               ),
             ),
-            const SizedBox(
-              height: 40,
-            ),
-            const Text('New User? Create Account')
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text('New User? Create Account')
+        ],
       ),
     );
   }
