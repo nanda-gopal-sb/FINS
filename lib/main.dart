24175354da23764aee791b941c3e993056b28a0d
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2024-04-17 11:43:16 +0530

    updated text fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:fins/app_theme.dart';
 import 'package:fins/page/home.dart';
-import 'package:fins/page/login.dart';
 import 'package:fins/page/signup.dart';
 import 'package:fins/provider/user_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
